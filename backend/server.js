c7fd679f4d524f5816094551ff86262cd45db426
Nabia-Sheikh <nabiasheikh2222@gmail.com>
2022-07-30 22:33:46 +0500

    Database Connection

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const dotenv = require("dotenv")
 const products = require("./data/products")
+const connectDB = require("./config/db")
+const colors = require("colors")
 
 const app = express()
 app.use(express.json())
 dotenv.config()
+connectDB()
 
 app.get("/api/products", (req, res) => {
   res.send(products)
@@ -18,5 +21,5 @@ app.get("/api/products/:id", (req, res) => {
 const port = process.env.PORT || 5000
 
 app.listen(port, () => {
-  console.log("Server is running on port " + port)
+  console.log(`Server is running on port ${port}`.yellow)
 })

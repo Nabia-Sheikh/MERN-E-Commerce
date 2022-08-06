4bd4a619fc120bc9b21d74c4ee817220c1954ca9
Nabia-Sheikh <78126275+Nabia-Sheikh@users.noreply.github.com>
2022-08-06 14:17:00 +0500

    place order screen added

--- frontend/src/screens/PlaceOrder.jsx
+++ frontend/src/screens/PlaceOrder.jsx
@@ -1,15 +1,17 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Link, useNavigate } from "react-router-dom"
 import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
 import Message from "../Component/Message"
 import CheckoutSteps from "../Component/CheckOutSteps"
+import { createOrder } from "../actions/orderAction"
 
 const PlaceOrder = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
 
   const cart = useSelector((state) => state.cart)
+  console.log(cart.cartItems)
 
   if (!cart.shippingAddress.address) {
     navigate("/shipping")
@@ -33,9 +35,27 @@ const PlaceOrder = () => {
     Number(cart.taxPrice)
   ).toFixed(2)
 
+  const orderCreate = useSelector((state) => state.orderCreate)
+  const { order, success, error } = orderCreate
+
+  useEffect(() => {
+    if (success) {
+      navigate(`/order/${order._id}`)
+    }
+    // eslint-disable-next-line
+  }, [navigate, success])
+
   const placeOrderHandler = () => {
     dispatch(
-     console.log("object")
+      createOrder({
+        orderItems: cart.cartItems,
+        shippingAddress: cart.shippingAddress,
+        paymentMethod: cart.paymentMethod,
+        itemsPrice: cart.itemsPrice,
+        shippingPrice: cart.shippingPrice,
+        taxPrice: cart.taxPrice,
+        totalPrice: cart.totalPrice,
+      })
     )
   }
 
@@ -71,12 +91,7 @@ const PlaceOrder = () => {
                     <ListGroup.Item key={index}>
                       <Row>
                         <Col md={1}>
-                          <Image
-                            src={item.img}
-                            alt={item.name}
-                            fluid
-                            rounded
-                          />
+                          <Image src={item.img} alt={item.name} fluid rounded />
                         </Col>
                         <Col>
                           <Link to={`/product/${item.product}`}>
@@ -124,9 +139,9 @@ const PlaceOrder = () => {
                   <Col>${cart.totalPrice}</Col>
                 </Row>
               </ListGroup.Item>
-              {/* <ListGroup.Item>
+              <ListGroup.Item>
                 {error && <Message variant="danger">{error}</Message>}
-              </ListGroup.Item> */}
+              </ListGroup.Item>
               <ListGroup.Item>
                 <Button
                   type="button"

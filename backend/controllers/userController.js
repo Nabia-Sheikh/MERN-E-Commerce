25bd8473c4b6c2a400875489f8463ba4e6e1cb7d
Nabia-Sheikh <nabiasheikh2222@gmail.com>
2022-08-12 18:09:28 +0500

    Password and Email Authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,10 +3,20 @@ const asyncHandler = require("express-async-handler")
 
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
-  res.send({
-    email,
-    password,
-  })
+  const user = await User.findOne({ email })
+  if (user && (await user.isValidPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    })
+  } else {
+    res.status(401).json({
+      message: "Invalid Credentials",
+    })
+  }
 })
 
 module.exports = { authUser }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,30 +1,36 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      uique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    uique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  isAdmin: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
-},
   {
     timestamps: true,
-})
+  }
+)
 
+userSchema.methods.isValidPassword = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
 const User = mongoose.model("User", userSchema)
 
 module.exports = User

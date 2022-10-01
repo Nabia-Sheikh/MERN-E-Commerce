fb11bd041f60e6c452cfb0627fdd4f0f2fcc0170
Nabia-Sheikh <78126275+Nabia-Sheikh@users.noreply.github.com>
2022-10-01 17:29:22 +0500

    deploying

--- backend/server.js
+++ backend/server.js
@@ -28,21 +28,21 @@ app.use("/api/upload", uploadRoutes)
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
-
+// const __dirname = path.resolve()
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/build")))
+if (process.env.NODE_ENV === "production") {
+  console.log("This is path",path.join(__dirname, "../frontend/build"))
+  app.use(express.static(path.join(__dirname, "../frontend/build")))
 
-//  app.get("*", function (req, res) {
-//    const index = path.join(__dirname, "build", "index.html")
-//    res.sendFile(index)
-//  })
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running....")
-//   })
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  )
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....")
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
